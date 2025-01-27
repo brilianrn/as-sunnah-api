c0446d9ff1b5057e4f0b86b2f3be02a96f17db07
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2025-01-27 17:55:12 +0700

    chore: prepare vercel

--- src/service/app.service.ts
+++ src/service/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): any {
     return {
-      name: 'Hello World masokk!',
+      name: 'Assalamualaikum!',
     };
   }
 }
